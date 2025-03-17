a2713e5b61211f91c0d1ce17ea44944d6e26bbee
James Koh <kohjianjie@gmail.com>
2025-03-17 23:13:40 +0800

    Basic UI functions

--- src/main/java/Game/Actions/StartAction.java
+++ src/main/java/Game/Actions/StartAction.java
@@ -1,4 +1,9 @@
 package Game.Actions;
 
+import Functionalities.Storage;
+import Functionalities.UI;
+import Game.Game;
+
 public class StartAction implements Action {
+    public void execute(Game game, Storage storage, UI ui) {}
 }
